7d978310ca3d6d057a7fdc58fa7820ac7204b004
xiaohong0409 <86586947+xiaohong0409@users.noreply.github.com>
2021-07-25 20:26:01 +0700

    Update function.php

--- function.php
+++ function.php
@@ -12,30 +12,5 @@ function pdo_connect_mysql() {
     }
 }
 
-function template_header($title) {
-echo <<<EOT
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>$title</title>
-		<link href="style.css" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-	</head>
-	<body>
-    <nav class="navtop">
-    	<div>
-    		<h1>Ticketing System</h1>
-            <a href="index.php"><i class="fas fa-ticket-alt"></i>Tickets</a>
-    	</div>
-    </nav>
-EOT;
-}
 
-function template_footer() {
-echo <<<EOT
-    </body>
-</html>
-EOT;
-}
 ?>
