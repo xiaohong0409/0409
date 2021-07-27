17af031178598d95883289aa6bd9b8d14688ef73
xiaohong0409 <86586947+xiaohong0409@users.noreply.github.com>
2021-07-27 16:56:36 +0700

    Update script.sql

--- script.sql
+++ script.sql
@@ -20,15 +20,28 @@ create table passenger
   primary key(bookingnumber)
 );
 
-
 create table tickets
 (
-  tocity varchar (20),
-  fromcity varchar(20),
-  date int,
-  email varchar(20),
-  bookingnumber int
-);
+	id int(11) NOT NULL AUTO_INCREMENT,
+	title varchar(255) NOT NULL,
+	msg text NOT NULL,
+	email varchar(255) NOT NULL,
+	created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	status enum('open','closed','resolved') NOT NULL DEFAULT 'open',
+	PRIMARY KEY (`id`)
+) 
+
+create table tickets_comments
+(
+	id int(11) NOT NULL AUTO_INCREMENT,
+	ticket_id int(11) NOT NULL,
+	msg text NOT NULL,
+	created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`)
+) 
+
+
+
 create table reservation
 (
   reservationnumber int,
@@ -46,3 +59,6 @@ create table bookedinfo
 insert into table passenger(fname,lname,address,email,bookingnumber) values('Xiaoming','Wang','123/3 Oak','xiaoming@gmail.com',001);
 insert into reservation(reservationnumber, seatnumber) values('09080','15');
 insert into bookedinfo(reservationnumber,passportnumber) values('09080','1038300');
+insert into tickets (id, title, msg, email, created, status) VALUES (1, 'Test Ticket', 'This is your first ticket.', 'support@codeshack.io', '2020-06-10 13:06:17', 'open');
+insert into tickets_comments (id, ticket_id, msg, created) VALUES (1, 1, 'This is a test comment.', '2020-06-10 16:23:39');
+
