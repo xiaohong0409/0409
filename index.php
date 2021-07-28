5215162e9771bcb65ff44861344734eb532e8011
xiaohong0409 <86586947+xiaohong0409@users.noreply.github.com>
2021-07-28 17:15:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -6,49 +8,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
-    <title></title>
+
+    <title>Bootstrap</title>
+
   </head>
   <body>
-    <include function.php>
+    <nav class="navbar navbar-expand-lg bg-success navbar-dark">
       <div class="container">
-      <h3>Add new passenger into database</h3>
-      <form action="process.php" method="post">
-        <div class="from-group">
-        <label>First Name</lable>
-        <input type="text" name="fname" class="form-control" value="">
-        <div class="from-group">
-        <lable>Last Name</lable>
-        <input type="text" name="lname" class="form-control" value="">
-        <div class="from-group">
-        <lable>Address</lable>
-        <input type="text" name="address" class="form-control" value="">
-        <div class="from-group">
-        <lable>Email</lable>
-        <input type="tetx" name="email" class="form-control" value="">
-        <div class="from-group">
-        <lable>Booking Number</lable>
-        <input type="text" name="bookingnumber" class="form-control" value="">
-      </div>
-      <div class="from-group">
-        <button type="submit" class="btn btn-primary" name="save">Save</button>
-      </div>
-                </form>
-    </div>
+<a href="#" class="navbar-brand">Booking System</a>
 
-            <?php
-            $conn=mysqli_connect("localhost","root","386465721","booking_system");
-            $query="select * from passenger";
-            $result=mysqli_query($conn,$query);
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
 
-            while($row=mysqli_fetch_array($result)){
-              echo"".$row['fname']." ".$row['lname']." ".$row['address']." ".$row['email']." ".$row['bookingnumber']."<br>";
-            }
-            ?>
-    </table>
-    </div>
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navmenu">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a href="home.php" class="nav-link">Home</a>
+    </li>
+		<li class="nav-item">
+			<a href="passenger.php" class="nav-link">Passenger</a>
+		</li>
+    <li class="nav-item">
+      <a href="help.php" class="nav-link">help</a>
+    </li>
+</div>
+</div>
+    </nav>
+
+</div>
 
 
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
+
 
