0fdf746e89bc2eadef4151ac3cacd541b73b032e
xiaohong0409 <86586947+xiaohong0409@users.noreply.github.com>
2021-07-29 17:24:27 +0700

    Update script.sql

--- script.sql
+++ script.sql
@@ -40,6 +40,6 @@ create table bookedinfo
 
 insert into reservation(reservationnumber, seatnumber) values('09080','15');
 insert into bookedinfo(reservationnumber,passportnumber) values('09080','1038300');
-INSERT INTO `train` (`trainno`, `tname`, `sp`, `st`, `dp`, `dt`, `dd`, `distance`) VALUES ('17', 'Haijian Express', 'Chandigarh', '10:00:00', 'Lucknow', '21:30:00', 'Day1', '700', '');
-INSERT INTO `passenger` (`fname`, `lname`, `address`, `email`, `bookingnumber`) VALUES ('Xiao', 'Li', '123/ Manhattan Condo', 'xiao@gmail.com', '123', '');
+INSERT INTO `train` (`trainno`, `tname`, `sp`, `st`, `dp`, `dt`, `dd`, `distance`) VALUES ('17', 'Haijian Express', 'Chandigarh', '10:00:00', 'Lucknow', '21:30:00', 'Day1', '700');
+INSERT INTO `passenger` (`fname`, `lname`, `address`, `email`, `bookingnumber`) VALUES ('Xiao', 'Li', '123/ Manhattan Condo', 'xiao@gmail.com', '123');
 
