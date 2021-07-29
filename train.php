740712e137c748ec2f84866f4c8c67532969cdfe
xiaohong0409 <86586947+xiaohong0409@users.noreply.github.com>
2021-07-29 13:23:11 +0700

    Update train.php

--- train.php
+++ train.php
@@ -1,26 +1,32 @@
 
+<html>
+<body style=" background-image: url(adminlogin.jpeg);
+    height: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;">
+
+
 <?php
+
 $conn=mysqli_connect("localhost","root","386465721","booking_system");
-if(!$conn){
-  echo 'Not connected';
-}
-if(!mysqli_select_db($conn,'booking_system')){
-  echo 'database not select';
-}
 
-$fname=$_POST['fname'];
-$lname=$_POST['lname'];
-$address=$_POST['address'];
-$email=$_POST['email'];
-$bookingnumber=$_POST['bookingnumber'];
-$sql="INSERT INTO passenger(fname,lname,address,email,bookingnumber) VALUES('$fname','$lname','$address','$email','$bookingnumber')";
+$query="SELECT * FROM train";
+$result=mysqli_query($conn,$query);
+echo "
+<table>
+<thead><td>Train_no</td><td>Train_name</td><td>Starting_Point</td><td>Arrival_Time</td><td>Destination_Point</td><td>Departure_Time</td><td>Day</td><td>Distance</td><td></td></thead>
+";
 
-if(mysqli_query($conn,$sql)){
-echo "New record saved successfully";
-}else{
-  echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
-  }
-mysqli_close($conn);
+while ($row=mysqli_fetch_array($result))
+{
+	echo "
+<tr><td>".$row['trainno']."</td><td>".$row['tname']."</td><td>".$row['sp']."</td><td>".$row['st']."</td><td>".$row['dp']."</td><td>".$row['dt']."</td><td>".$row['dd']."</td><td>".$row['distance']."";
+}
+echo "</table>";
+
 
 
 ?>
+</body>
+</html>
